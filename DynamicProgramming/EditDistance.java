e0c6073dbffe79b6e39a5f713c5cdc221092473f
Raunak Bhupal <43487979+raunakbhupal@users.noreply.github.com>
2020-08-04 20:52:12 +0530

    Edit Distance
    Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to convert ‘str1′ into ‘str2′.
    
    Insert
    Remove
    Replace
    All of the above operations are of cost=1.
    Both the strings are of lowercase.
    
    The First line of the input contains an integer T denoting the number of test cases. Then T test cases follow. Each tese case consists of two lines. The first line of each test case consists of two space separated integers P and Q denoting the length of the strings str1 and str2 respectively. The second line of each test case coantains two space separated strings str1 and str2 in order.

--- DynamicProgramming/EditDistance.java
+++ DynamicProgramming/EditDistance.java
@@ -1,63 +1,43 @@
+/*package whatever //do not write package name here */
+
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class PrimitiveCalculator {
-
-    private static List<Integer> optimal_sequence(int n) {
-        List<Integer> sequence = new ArrayList<Integer>();
-	HashMap<Integer,Integer> map=new HashMap<>();
-	map.put(0,0);
-	int i=1,v3,v2,v1;
-	while(i<=n){
-		if(i%3==0)
-			v3=map.get(i/3);
-		else
-			v3=n;
-		if(i%2==0)
-			v2=map.get(i/2);
-		else
-			v2=n;
-		v1=map.get(i-1);
-		int val;
-		if(v3<=v2&&v3<=v1)
-			val=i/3;
-		else if(v2<=v3&&v2<=v1)
-			val=i/2;
-		else
-			val=i-1;
-		int min=map.get(val)+1;
-		map.put(i,min);
-		i++;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int m=sc.nextInt();
+		    int n=sc.nextInt();
+		    String s1=sc.next();
+		    String s2=sc.next();
+		    editDistance(s1,s2,m,n);
+		}
 	}
-	while(n>0){
-		sequence.add(n);
-		if(n%3==0)
-			v3=map.get(n/3);
-		else
-			v3=n;
-		if(n%2==0)
-			v2=map.get(n/2);
-		else
-
-			v2=n;
-		v1=map.get(n-1);
-		if(v3<=v2&&v3<=v1)
-			n/=3;
-		else if(v2<=v3&&v2<=v1)
-			n/=2;
-		else
-			n-=1;
+	static void editDistance(String s1,String s2,int m,int n){
+	    int dp[][]=new int[m+1][n+1];
+	    for(int i=0;i<=m;i++){
+	        for(int j=0;j<=n;j++){
+	            if(i==0)
+	                dp[i][j]=j;
+	            else if(j==0)
+	                dp[i][j]=i;
+	            else if(s1.charAt(i-1)==s2.charAt(j-1))
+	                dp[i][j]=dp[i-1][j-1];
+	            else
+	                dp[i][j]=1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]);
+	        }
+	    }
+	    System.out.println(dp[m][n]);
 	}
-        Collections.reverse(sequence);
-        return sequence;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        List<Integer> sequence = optimal_sequence(n);
-        System.out.println(sequence.size() - 1);
-        for (Integer x : sequence) {
-            System.out.print(x + " ");
-        }
-    }
+	static int min(int a,int b,int c){
+	    if(a<=b&&a<=c)
+	        return a;
+	    else if(b<=a&&b<=c)
+	        return b;
+	    else
+	        return c;
+	}
 }
