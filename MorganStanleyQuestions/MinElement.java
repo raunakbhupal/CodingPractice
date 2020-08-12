e4d987fb1af510a4e369560bf2cc9ae44db4c8d8
Raunak Bhupal <43487979+raunakbhupal@users.noreply.github.com>
2020-08-12 23:35:44 +0530

    Minimum element in a sorted and rotated array
    A sorted array A[ ] with distinct elements is rotated at some unknown point, the task is to find the minimum element in it.
    
    Expected Time Complexity: O(Log n)
    
    Input:
    
    The first line of input contains a single integer T denoting the number of test cases. Then T test cases follow. Each test case consist of two lines. The first line of each test case consists of an integer N, where N is the size of array.
    The second line of each test case contains N space separated integers denoting array elements.
    
    Output:
    Corresponding to each test case, in a new line, print the minimum element in the array.
    
    Constraints:
    
    1 ≤ T ≤ 200
    1 ≤ N ≤ 500
    1 ≤ A[i] ≤ 1000
    
    Example:
    
    Input
    2
    5
    4 5 1 2 3
    6
    10 20 30 40 50 5 7
    
    Output
    1
    5

---
+++ MorganStanleyQuestions/MinElement.java
@@ -0,0 +1,31 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int n=sc.nextInt();
+		    int a[]=new int[n];
+		    for(int i=0;i<n;i++)
+		        a[i]=sc.nextInt();
+		    System.out.println(findMin(a,0,n-1));
+		}
+	}
+	static int findMin(int a[],int l,int h){
+	    while(l<h){
+	        int mid=l+(h-l)/2;
+	        if(a[mid]==a[h])
+	            h--;
+	        else if(a[mid]>a[h])
+	            l=mid+1;
+	        else
+	            h=mid;
+	    }
+	    return a[h];
+	}
+}
