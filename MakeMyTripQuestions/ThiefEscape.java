55704164f11916de7cb4fd835b62d72f667c389d
Raunak Bhupal <43487979+raunakbhupal@users.noreply.github.com>
2020-08-30 00:00:02 +0530

    Total jumps required for the thief to escape
    A thief trying to escape from a jail has to cross N walls each with varying heights. He climbs X feet every time. But, due to the slippery nature of those walls, every times he slips back by Y feet.  Now the task is to calculate the total number of jumps required to cross all walls and escape from the jail.
    
    Input:
    The first line of input contains an integer T denoting the no of test cases. Then T test cases follow. Each test case contains three space separated integers X, Y, N. Then in the next line are N space separated values denoting the heights ( Ht[] ) of the walls.
    
    Output:
    For each test case in a new line print the total number of jumps.
    
    Constraints:
    1<=T<=100
    1<= N, X, Y <=100
    1<= Ht[] <=1000
    
    Example:
    Input:
    2
    10 1 1
    5
    4 1 5
    6 9 11 4 5 
    Output:
    1
    12

---
+++ MakeMyTripQuestions/ThiefEscape.java
@@ -0,0 +1,39 @@
+// Java program to find the number of 
+// jumps required 
+public class GFG { 
+	
+	/* function to calculate jumps required 
+		to cross walls */
+	static int jumpcount(int x, int y, int n, 
+								int height[]) 
+	{ 
+		int jumps = 0; 
+	
+		for (int i = 0; i < n; i++) { 
+			if (height[i] <= x) { 
+				jumps++; 
+				continue; 
+			} 
+	
+			/* If height of wall is greater than 
+			up move */
+			int h = height[i]; 
+			while (h > x) 
+			{ 
+				jumps++; 
+				h = h - (x - y); 
+			} 
+			jumps++; 
+		} 
+		return jumps; 
+	} 
+	
+	/*driver function*/
+	public static void main(String args[]) 
+	{ 
+		int x = 10, y = 1; 
+		int height[] = { 11, 10, 10, 9 }; 
+		int n = height.length; 
+		System.out.println(jumpcount(x, y, n, height)); 
+	} 
+} 
