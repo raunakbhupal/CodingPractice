b20c6734504143669486895d440a41d344341cc8
Raunak Bhupal <raunakbhupal7@gmail.com>
2021-05-13 01:39:59 +0530

    Added a problem variant

--- Arrays/StockBuySell.java
+++ Arrays/StockBuySell.java
@@ -73,3 +73,29 @@ class StockBuySell {
 		stock.stockBuySell(price, n); 
 	} 
 } 
+
+
+
+
+
+//Code for when you can buy and sell only once and need to find the max profit
+
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+}
+
+/*Example
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.*/
