201f679e6107b1d30122d6036e22dcdda3663734
Raunak Bhupal <raunakbhupal7@gmail.com>
2023-12-16 18:36:59 -0500

    Create PreorderTraversalIterative.java
    Given the root of a binary tree, return the preorder traversal of its nodes' values.

---
+++ TreesNew/PreorderTraversalIterative.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while(!stack.isEmpty()) {
+            root = stack.pop();
+            list.add(root.val);
+            if (root.right != null) {
+                stack.add(root.right);
+            }
+            if (root.left != null) {
+                stack.add(root.left);
+            }
+        }
+        return list;
+    }
+}
