fdbeeea75735739bb2537ee9498c0d53e995b994
Raunak Bhupal <raunakbhupal7@gmail.com>
2023-12-18 11:32:45 -0500

    Create BottomLeftTreeValue.java
    Given the root of a binary tree, return the leftmost value in the last row of the tree.

---
+++ TreesNew/BottomLeftTreeValue.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int leftVal = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            leftVal = queue.peek().val;
+            while(size > 0) {
+                root = queue.poll();
+                if (root.left != null) {
+                    queue.add(root.left);
+                }
+                if (root.right != null) {
+                    queue.add(root.right);
+                }
+                size--;
+            }
+        }
+        return leftVal;
+    }
+}
