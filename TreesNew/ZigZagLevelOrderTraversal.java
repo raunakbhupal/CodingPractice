5b2fed2377911a939625a3100083aa4a8d5fb221
Raunak Bhupal <raunakbhupal7@gmail.com>
2023-12-17 23:12:49 -0500

    Create ZigZagLevelOrderTraversal.java
    Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).

---
+++ TreesNew/ZigZagLevelOrderTraversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean flag = false;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list1 = new ArrayList<>();
+            while (size > 0) {
+                root = queue.poll();
+                list1.add(root.val);
+                if (root.left != null) {
+                    queue.add(root.left);
+                }
+                if (root.right != null) {
+                    queue.add(root.right);
+                }
+                size--;
+            }
+            if (flag) {
+                Collections.reverse(list1);
+            }
+            list.add(list1);
+            flag = !flag;
+        }
+        return list;
+    }
+}
