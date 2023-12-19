ad6e6f9bfc46e30638020d21699648a3c72a0e13
Raunak Bhupal <raunakbhupal7@gmail.com>
2023-12-18 22:16:49 -0500

    Create RightSideView.java
    Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.

---
+++ TreesNew/RightSideView.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int temp = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                root = queue.poll();
+                temp = root.val;
+                if (root.left != null) {
+                    queue.add(root.left);
+                }
+                if (root.right != null) {
+                    queue.add(root.right);
+                }
+                size--;
+            }
+            list.add(temp);
+        }
+        return list;
+    }
+}
