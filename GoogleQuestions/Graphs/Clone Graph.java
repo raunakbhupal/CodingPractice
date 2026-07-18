b87eb96524ee73bff2c1379951ee6c62669f1620
Raunak Bhupal <raunakbhupal7@gmail.com>
2026-07-18 16:43:04 -0400

    Implement graph cloning in Clone Graph.java
    Given a node in a connected undirected graph, return a deep copy of the graph.
    
    Each node in the graph contains an integer value and a list of its neighbors.
    
    class Node {
        public int val;
        public List<Node> neighbors;
    }
    The graph is shown in the test cases as an adjacency list. An adjacency list is a mapping of nodes to lists, used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
    
    For simplicity, nodes values are numbered from 1 to n, where n is the total number of nodes in the graph. The index of each node within the adjacency list is the same as the node's value (1-indexed).
    
    The input node will always be the first node in the graph and have 1 as the value.

---
+++ GoogleQuestions/Graphs/Clone Graph.java
@@ -0,0 +1,36 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Node,Node> map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        
+        if (map.containsKey(node)) return map.get(node);  // guard at the top
+        
+        Node current = new Node(node.val);
+        map.put(node, current);
+        
+        for (Node nei : node.neighbors) {
+            current.neighbors.add(cloneGraph(nei));  // no guard needed here
+        }
+        return current;
+    }
+}
